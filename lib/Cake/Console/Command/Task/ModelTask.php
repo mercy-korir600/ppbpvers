cakephp/cakephp

commit eff4004261a2bd05e4df54ee2af057c680ff50fc

Don't treat tables that substring match as associations
The table structure of:

- packages
- userpackages
- georegions
- userpackages_georegions

Should not create a habtm between packages & georegions.

Fixes #3532

lib/Cake/Console/Command/Task/ModelTask.php
@@ -627,7 +627,7 @@ class ModelTask extends BakeTask {
 			$offset = strpos($otherTable, $model->table . '_');
 			$otherOffset = strpos($otherTable, '_' . $model->table);
 
-			if ($offset !== false) {
+			if ($offset === 0) {
 				$offset = strlen($model->table . '_');
 				$habtmName = $this->_modelName(substr($otherTable, $offset));
 				$associations['hasAndBelongsToMany'][] = array(
@@ -637,7 +637,7 @@ class ModelTask extends BakeTask {
 					'associationForeignKey' => $this->_modelKey($habtmName),
 					'joinTable' => $otherTable
 				);
-			} elseif ($otherOffset !== false) {
+			} elseif ($otherOffset === 0) {
 				$habtmName = $this->_modelName(substr($otherTable, 0, $otherOffset));
 				$associations['hasAndBelongsToMany'][] = array(
 					'alias' => $habtmName,

mercy-korir600/ppbpvers

commit 201f66dda00452ba54a72f7f8b6bde2f50a82bd3

Fix issue where bake was missing habtm associations
If the current model is not the first table, habtm associations would be
missed. This was caused by eff4004261a2bd05e4df54ee2af057c680ff50fc

Refs #3532
Fixes #3605

lib/Cake/Console/Command/Task/ModelTask.php
@@ -623,9 +623,9 @@ class ModelTask extends BakeTask {
 			$offset = strpos($otherTable, $model->table . '_');
 			$otherOffset = strpos($otherTable, '_' . $model->table);
 
-			if ($offset === 0) {
+			if ($offset !== false) {
 				$tableName = substr($otherTable, strlen($model->table . '_'));
-			} elseif ($otherOffset === 0) {
+			} elseif ($otherOffset !== false) {
 				$tableName = substr($otherTable, 0, $otherOffset);
 			}
 			if ($tableName && in_array($tableName, $this->_tables)) {