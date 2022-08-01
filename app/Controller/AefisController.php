8d49f4a0e98e943c9d5651734745e9b048150428
Itskiprotich <keeprawteachjapheth@gmail.com>
2022-08-01 05:33:10 +0000

    xml->json

--- app/Controller/AefisController.php
+++ app/Controller/AefisController.php
@@ -97,7 +97,7 @@ class AefisController extends AppController {
          if ($results->isOk()) {
             $body = $results->body;
             $resp = json_decode($body, true);
-            $this->Aefi->saveField('webradr_message', $body);
+            // $this->Aefi->saveField('webradr_message', $body);
            $this->Aefi->saveField('webradr_date', date('Y-m-d H:i:s'));
            $this->Aefi->saveField('webradr_ref', $resp['report']['id']);
             $this->Flash->success('Yello Card Scheme integration success!!');
@@ -106,14 +106,14 @@ class AefisController extends AppController {
 
          }else{
             $body = $results->body;
-                $this->Aefi->saveField('webradr_message', $body);
+                // $this->Aefi->saveField('webradr_message', $body);
                 $this->Flash->error('Error sending report to Yello Card Scheme:');
                 $this->Flash->error($body);
                 $this->redirect($this->referer());
          }
         }else{
             $body = $initiate->body;
-            $this->Aefi->saveField('webradr_message', $body);
+            // $this->Aefi->saveField('webradr_message', $body);
             $this->Flash->error('Error sending report to Yello Card Scheme:');
             $this->Flash->error($body);
             $this->redirect($this->referer());
