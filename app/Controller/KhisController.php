9057c1537b9fe1f7f9ecce516499e012bc53b200
Itskiprotich <keeprawteachjapheth@gmail.com>
2023-11-01 06:10:48 +0000

    khis

---
+++ app/Controller/KhisController.php
@@ -0,0 +1,27 @@
+<?php
+App::uses('AppController', 'Controller');
+App::uses('AppController', 'Controller');
+App::uses('HttpSocket', 'Network/Http');
+/**
+ * Khis Controller
+ */
+class KhisController extends AppController
+{
+
+	/**
+	 * Scaffold
+	 *
+	 * @var mixed
+	 */
+	public $scaffold;
+
+	public function beforeFilter()
+	{
+		parent::beforeFilter();
+	}
+
+	public function manager_index()
+	{
+		 
+	}
+}
