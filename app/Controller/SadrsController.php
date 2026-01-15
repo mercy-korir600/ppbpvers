118ae8c5ca78faa2ffc0faf197334ea832e11840
itsjkiprotich <keeprawteachjapheth@gmail.com>
2026-01-15 19:26:25 +0000

    UPD: fix vigiflow header content type

--- app/Controller/SadrsController.php
+++ app/Controller/SadrsController.php
@@ -969,7 +969,10 @@ $this->manager_index();
         $results = $HttpSocket->post(
             Configure::read('vigiflow_api'),
             $html,
-            array('header' => array('umc-vigiflow-web-radr-access-key' => Configure::read('vigiflow_key')))
+            array('header' => array(
+                'umc-vigiflow-web-radr-access-key' => Configure::read('vigiflow_key'),
+                'Content-Type' => 'application/xml'
+            ))
         );
 
         if ($results->isOk()) {
